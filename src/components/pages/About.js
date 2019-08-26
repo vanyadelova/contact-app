4e1dd2de62b821ab5f4bf909e232e9bc600f856b
vanyadelova <happy.vanya.delova@gmail.com>
2019-08-26 10:10:18 +0200

    fixinf the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default () => {
     <div>
       <h1 className="display-4">About Contact App</h1>
       <p className="lead">Simple app to manage contacts</p>
-      <p className="text-secondary">Version 1.0.0</p>
+      
     </div>
   );
 };
